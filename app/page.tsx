72388c8d6912d2dc011ccb77ef9caef503b474be
Thomas <thomastian2041@gmail.com>
2023-07-12 15:49:58 +0800

    minor fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ const Home = () => {
     <section className="flex-col mb-16 flex-start paddings">
       <h1>Categories</h1>
       <h1>Posts</h1>
-      <h1>LoadMore</h1>
+      <h1>Load More</h1>
     </section>
   );
 };
