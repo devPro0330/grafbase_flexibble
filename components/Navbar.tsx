9623489604b2e7dc5dff039b6d3d2c5695b52b70
Thomas <thomastian2041@gmail.com>
2023-07-13 16:05:29 +0800

    configed jwt authentication

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,9 +3,10 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import AuthProviders from "./AuthProviders";
+import { getCurrentUser } from "@/lib/session";
 
-const Navbar = () => {
-  const session = {};
+const Navbar = async () => {
+  const session = await getCurrentUser();
   return (
     <nav className="flexBetween navbar">
       <div className="flex-1 gap-10 flexStart">
@@ -22,9 +23,17 @@ const Navbar = () => {
       </div>
 
       <div className="gap-4 flexCenter">
-        {session ? (
+        {session?.user ? (
           <>
-            UserPhoto
+            {session?.user?.image && (
+              <Image
+                src={session.user.image}
+                width={40}
+                height={40}
+                className="rounded-full"
+                alt={session.user.name}
+              />
+            )}
             <Link href="/create-project">Share Work</Link>
           </>
         ) : (
