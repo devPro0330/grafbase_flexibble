80b7c998031bd67fa8f64faf2dca2d49ba725f32
Thomas <thomastian2041@gmail.com>
2023-07-18 09:45:36 +0800

    integrated create & edit project with api

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -7,6 +7,8 @@ import FormField from "./FormField";
 import { categoryFilters } from "@/constants";
 import CustomMenu from "./CustomMenu";
 import Button from "./Button";
+import { createNewProject, fetchToken } from "@/lib/actions";
+import { useRouter } from "next/navigation";
 
 type Props = {
   type: string;
@@ -14,6 +16,7 @@ type Props = {
 };
 
 const ProjectForm = ({ type, session }: Props) => {
+  const router = useRouter();
   const [form, setForm] = useState({
     title: "",
     description: "",
@@ -23,7 +26,25 @@ const ProjectForm = ({ type, session }: Props) => {
     category: "",
   });
   const [isSubmitting, setIsSubmitting] = useState(false);
-  const handleFormSubmit = (e: React.FormEvent) => {};
+  const handleFormSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    setIsSubmitting(true);
+
+    const { token } = await fetchToken();
+
+    try {
+      if (type === "create") {
+        await createNewProject(form, session?.user?.id, token);
+
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
   const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
 
@@ -117,9 +138,13 @@ const ProjectForm = ({ type, session }: Props) => {
 
       <div className="w-full flexStart">
         <Button
-          title="Create"
+          title={
+            isSubmitting
+              ? `${type === "create" ? "Creating" : "Editing"}`
+              : `${type === "create" ? "Create" : "Edit"}`
+          }
           type="submit"
-          LeftIcon={isSubmitting ? "" : "/plus.svg"}
+          leftIcon={isSubmitting ? "" : "/plus.svg"}
           isSubmitting={isSubmitting}
         />
       </div>
